fb4c1a209d3842d3dfcda3e08d0ce160d9de2e4a
AliZamani696 <azamani081@gmail.com>
2025-11-29 13:15:39 +0330

    register product and category in admin panel

--- Core/Store/admin.py
+++ Core/Store/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category,Product
+
+
+
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('CategoryName', 'CategoryParent', 'has_children')
+    list_filter = ('CategoryParent', )
+    search_fields = ('name',)
+    
+@admin.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('ProductName', 'ProductCategory', 'ProductDescription',"ProductPrice")
+    list_filter = ('ProductPrice','ProductCategory','ProductName' )
+    search_fields = ('ProductName',)
+    
